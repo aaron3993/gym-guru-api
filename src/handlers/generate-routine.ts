145c26600d90ef6597fb34e52690466f903a05a6
Aaron <aaron3993@hotmail.com>
2025-02-02 19:09:57 -0500

    able to make post request with client

--- src/handlers/generate-routine.ts
+++ src/handlers/generate-routine.ts
@@ -1,7 +1,12 @@
 export const handler = async () => {
     return {
-        'statusCode': 200,
-        'headers': {'Content-Type': 'application/json'},
-        'body': JSON.stringify({ message: 'Hello World!'})
+        statusCode: 200,
+        headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "http://localhost:3000", // Allow any origin
+            "Access-Control-Allow-Methods": "POST, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        },
+        body: JSON.stringify({ message: 'Hello World!'})
       }
 }
