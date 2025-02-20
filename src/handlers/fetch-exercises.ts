c87a4566498d267b7e5c7e28c7ae6c06fed0b9a0
Aaron <aaron3993@hotmail.com>
2025-02-20 18:44:59 -0500

    added error handling, removed log

--- src/handlers/fetch-exercises.ts
+++ src/handlers/fetch-exercises.ts
@@ -8,52 +8,58 @@ export const handler: APIGatewayProxyHandler = async (event) => {
 
     const allowedOrigins: string[] = ["http://localhost:3000", "https://gymguru-37ed9.web.app"];
 
-  try {
-    if (!event.headers?.Authorization) {
-        return {
-          statusCode: 400,
-          headers: {
-              "Content-Type": "application/json",
-              "Access-Control-Allow-Origin": origin && allowedOrigins.includes(origin) ? origin : "https://gymguru-37ed9.web.app/",
-              "Access-Control-Allow-Methods": "POST, OPTIONS",
-              "Access-Control-Allow-Headers": "Content-Type, Authorization",
-              "Access-Control-Allow-Credentials": "true",
-          },
-        body: JSON.stringify({ message: 'Authorization header missing' }),
+    try {
+        if (!event.headers?.Authorization) {
+            return {
+                statusCode: 400,
+                headers: {
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Origin": origin && allowedOrigins.includes(origin) ? origin : "https://gymguru-37ed9.web.app/",
+                    "Access-Control-Allow-Methods": "POST, OPTIONS",
+                    "Access-Control-Allow-Headers": "Content-Type, Authorization",
+                    "Access-Control-Allow-Credentials": "true",
+                },
+                body: JSON.stringify({ message: 'Authorization header missing' }),
+            };
+        }
+    
+        await initializeFirebase()
+
+        const token = event.headers.Authorization?.split('Bearer ')[1];
+
+        if (!token) {
+            return {
+                statusCode: 403,
+                body: JSON.stringify({ message: 'Unauthorized' }),
+            };
+        }
+
+        await verifyToken(token);
+
+        const rapidApiKeyString = "rapid-api-key";
+        const rapidApiKey = await getSSMParameter(rapidApiKeyString);
+        if (!rapidApiKey) throw new Error("Rapid API Key not found");
+
+        const options = {
+            params: { limit: "1323" },
+            headers: {
+                "X-RapidAPI-Key": rapidApiKey,
+                "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+            },
         };
-    }
-    
-    await initializeFirebase()
 
-    const token = event.headers.Authorization?.split('Bearer ')[1];
-    
-    await verifyToken(token);
-
-    const rapidApiKeyString = "rapid-api-key";
-    const rapidApiKey = await getSSMParameter(rapidApiKeyString);
-    if (!rapidApiKey) throw new Error("Rapid API Key not found");
-
-    const options = {
-      params: { limit: "1323" },
-      headers: {
-        "X-RapidAPI-Key": rapidApiKey,
-        "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
-      },
-    };
-
-    const response = await axios.get("https://exercisedb.p.rapidapi.com/exercises", options);
-    console.log('fetch exercises lambda done')
-    return {
-      statusCode: 200,
-      body: JSON.stringify(response.data),
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": origin && allowedOrigins.includes(origin) ? origin : "https://gymguru-37ed9.web.app/",
-        "Access-Control-Allow-Methods": "POST, OPTIONS",
-        "Access-Control-Allow-Headers": "Content-Type, Authorization",
-        "Access-Control-Allow-Credentials": "true",
-        },
-    };
+        const response = await axios.get("https://exercisedb.p.rapidapi.com/exercises", options);
+        return {
+            statusCode: 200,
+            body: JSON.stringify(response.data),
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": origin && allowedOrigins.includes(origin) ? origin : "https://gymguru-37ed9.web.app/",
+                "Access-Control-Allow-Methods": "POST, OPTIONS",
+                "Access-Control-Allow-Headers": "Content-Type, Authorization",
+                "Access-Control-Allow-Credentials": "true",
+            },
+        };
     } catch (error) {
         console.error("Error in fetchAllExercises:", error);
         return {
